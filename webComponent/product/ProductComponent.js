7a94bbe0a4b5214dd4dfb470c95944cbbaebbd9b
Michael-Chacon <michaelchacon02@gmail.com>
2024-08-09 10:05:25 -0500

    feat: :sparkles: ProductComponent ya filtra por gama

--- webComponent/product/ProductComponent.js
+++ webComponent/product/ProductComponent.js
@@ -35,7 +35,7 @@ export class ProductComponent extends HTMLElement {
   }
 
   render() {
-    this.innerHTML = /*html*/`
+    this.innerHTML = /*html*/ `
           <div class="container " style="margin-top: 20px;">
           <div class="row padre">
               <div class="col-12 hija2 shadow p-3 mb-5 bg-body rounded">
@@ -126,9 +126,17 @@ export class ProductComponent extends HTMLElement {
   // export function createImput(elementoPadre, iddinamico, tipo, nombre, subtexto, etiqueta, hidden)
 
   async llenarFormulario() {
-    this.gama = await getData("api/productRanges")
-    createSelect(this.filtroGama,"", "rangeCode", "", this.gama.data)
-    createImput(this.filtroStock, "", "number", "stockQuantity", "Filtrar por esta cantidad", "input", false)
+    this.gama = await getData("api/productRanges");
+    createSelect(this.filtroGama, "", "rangeCode", "", this.gama.data);
+    createImput(
+      this.filtroStock,
+      "",
+      "number",
+      "stockQuantity",
+      "Filtrar por esta cantidad",
+      "input",
+      false
+    );
 
     createImput(this.formulario, "", "text", "id", "", "input", true);
 
@@ -170,6 +178,7 @@ export class ProductComponent extends HTMLElement {
       "input"
     );
 
+    // select range code
     const selectCliente = document.createElement("div");
     selectCliente.innerHTML = `
     <label for="rangeCode" class="form-label mt-3">rangeCode</label>
@@ -186,7 +195,6 @@ export class ProductComponent extends HTMLElement {
       option.textContent = item.name;
       padreCliente.appendChild(option);
     });
-    // createSelect(this.formulario, "", "rangeCode", "Office code", this.gama.data);
 
     const botones = document.createElement("div");
     botones.innerHTML = `
@@ -222,9 +230,9 @@ export class ProductComponent extends HTMLElement {
 
       const inputs = new FormData(this.formulario);
       const data = Object.fromEntries(inputs);
-    
-      console.log(data)
-      data.rangeCode = {"id": parseInt(data.rangeCode)}
+
+      console.log(data);
+      data.rangeCode = { id: parseInt(data.rangeCode) };
       if (data.id !== "") {
         const respuesta = await updateData(data, this.endPoint, data.id);
         console.log(respuesta.status);
@@ -236,12 +244,12 @@ export class ProductComponent extends HTMLElement {
       }
       manipularModal(this.modal, "hide");
       alertaTemporal(this.alerta, "Successful process", "success");
-      this.tabla();
+      this.filtro();
       this.formulario.reset();
     });
   }
 
-  async filtro(){
+  async filtro() {
     const info = await getData(this.endPoint, "");
     this.datos = info.data;
 
@@ -254,6 +262,7 @@ export class ProductComponent extends HTMLElement {
     });
     this.tabla();
 
+    // Filter for range code
     const filtroPorGama = document.querySelector("#gamaProducto");
     filtroPorGama.addEventListener("submit", async (e) => {
       e.preventDefault();
@@ -262,20 +271,17 @@ export class ProductComponent extends HTMLElement {
       console.log(obj);
       filtroPorGama.reset();
       manipularModal(document.querySelector("#filtroGama"), "hide");
-      const filtro = await getData("api/customers/by-city/" + obj.city);
+      const filtro = await getData("api/products/by-range/" + obj.rangeCode);
       console.log(filtro.data);
       const convertedData = filtro.data.map((item) => {
         return {
           id: item[0],
-          firstName: item[1],
-          lastName1: item[2],
-          lastName2: item[3],
-          email: item[4],
-          city: item[5],
-          addressLine1: item[6].addressLine1,
-          addressLine2: item[6].addressLine2,
-          number: 1234,
-          salesRep: item[7].id,
+          name: item[1],
+          stockQuantity: item[2],
+          salePrice: item[3],
+          rangeCode: item[4],
+          productDescription: item[5],
+          dimensions: item[6],
         };
       });
       console.log(convertedData);
@@ -286,15 +292,15 @@ export class ProductComponent extends HTMLElement {
 
   async tabla() {
     const contenedor = document.querySelector(".contenedor");
-    this.datos = await getData(this.endPoint, "");
-    if (this.datos.data.length === 0) {
+    // this.datos = await getData(this.endPoint, "");
+    if (this.datos.length === 0) {
       alertaGenerica("No registered status ", contenedor);
     } else {
       contenedor.innerHTML = "";
     }
     const cuerpoTabal = document.querySelector("#info-tabla");
     cuerpoTabal.innerHTML = "";
-    this.datos.data.forEach((dato) => {
+    this.datos.forEach((dato) => {
       const { name, stockQuantity, id, salePrice, rangeCode } = dato;
       cuerpoTabal.innerHTML += /*html*/ `
                 <tr>
@@ -317,13 +323,14 @@ export class ProductComponent extends HTMLElement {
       const id = e.target.id;
       if (e.target.classList.contains("editar")) {
         const objeto = await this.buscarObjecto(id);
-        objeto.rangeCode = objeto.rangeCode.id
+        objeto.rangeCode = objeto.rangeCode.id;
+        console.log(objeto);
         poblarFormulario(objeto, this.formulario, this.modal);
       } else if (e.target.classList.contains("eliminar")) {
         if (pedirConfirmacion("este producto")) {
           await deleteData(id, this.endPoint);
           alertaTemporal(this.alerta, "Eliminado correctacmente", "info");
-          this.tabla();
+          this.filtro();
         }
       } else {
         console.log("Este elemento no tiene la clase");
